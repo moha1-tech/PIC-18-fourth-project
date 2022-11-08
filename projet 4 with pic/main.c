0214fe478888d7491e24aee50a12c99ebc9437d6
SOUFI Mohamed Taha <73140531+moha1-tech@users.noreply.github.com>
2022-11-08 01:31:30 +0100

    Add files via upload

---
+++ projet 4 with pic/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pic18f45k22.h>
+#include <config.h>         // Copy config.h to C:\Program Files (x86)\Microchip\xc8\v2.00\pic\include
+#include <xc.h>
+
+#define _XTAL_FREQ  8000000
+
+            
+void main(void) {
+    
+}
+
